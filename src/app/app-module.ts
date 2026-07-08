225c913c5749778590c90baa88ddf2b3d3777607
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2026-07-07 22:32:51 -0600

    Adding search component, search routing and app works new elements

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -2,16 +2,23 @@ import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { App } from './app';
-import { CoreModule } from "./core/core-module";
+import { CoreModule } from './core/core-module';
+import { AppRoutingModule } from './app-routing-module';
+import { HomeModule } from './pages/home/home-module';
+import { SearchModule } from './pages/search/search-module';
 
 @NgModule({
   declarations: [App],
   imports: [
-    BrowserModule, 
+    BrowserModule,
 
     //own modules
-    CoreModule
-  
+    CoreModule,
+
+    //no home routing module because it is lazy loaded
+    HomeModule,
+    SearchModule,
+    AppRoutingModule,
   ],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
