260566be41b61a6ccca82bc6f27d54ffdad45fbe
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2026-07-08 18:21:52 -0600

    working with ngStyle and NgClass with directives

--- src/app/shared/shared-module.ts
+++ src/app/shared/shared-module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 
+import { EntranceRevealDirective } from './directives/entrance-reveal-directive';
+import { MagneticTiltDirective } from './directives/magnetic-tilt-directive';
 import { RevealOnInteractionDirective } from './directives/reveal-on-interaction-directive';
 import { TypeColorDirective } from './directives/type-color-directive';
 import { PokedexIdPipe } from './pipes/pokedex-id-pipe';
@@ -11,12 +13,16 @@ import { PokemonTypeColorPipe } from './pipes/pokemon-type-color-pipe';
     PokemonTypeColorPipe,
     TypeColorDirective,
     RevealOnInteractionDirective,
+    EntranceRevealDirective,
+    MagneticTiltDirective,
   ],
   exports: [
     PokedexIdPipe,
     PokemonTypeColorPipe,
     TypeColorDirective,
     RevealOnInteractionDirective,
+    EntranceRevealDirective,
+    MagneticTiltDirective,
   ],
 })
 export class SharedModule {}
